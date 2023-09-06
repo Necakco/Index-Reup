436a6d70a7da3153c2a0bc7bdad248aa8635bc30
Necakco <khoavencyti@gmail.com>
2023-09-06 19:06:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Mentorship Guide
+
+Read the guide: [https://mentorship.guide](https://mentorship.guide)
+
+[![PRs Welcome](https://img.shields.io/badge/prs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+## 📖 About
+
+Mentorship Guide is a resource focused on making mentorship accessible to all.
+
+## ✨ Mission
+
+To provide mentors and mentees with strategies, resources, and community support.
+
+## 💖 Why
+
+The more mentorship we have, the more inclusive, helpful, collaborative, and innovative our communities become.
+
+## ✏ Contributing
+
+This guide is developed by the community for the community. We'd be thankful and honored to have you contribute and make the Mentorship Guide better than it is today!
+
+Please see the [contributing guide](/docs/contributing.md) for how to do so.
+
+## 🧬 Code of Conduct
+
+Our community is open, welcoming, and inclusive.
+
+This project adheres to the Contributor Covenant Code of Conduct. Please read and follow our [Code of Conduct](docs/code-of-conduct.md).
+
+## 👏 Thank you
+
+Together, we're stronger. Thank you for your contributions!
+
+## 📄 License
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+
+## 🚀 What's next?
+
+Short term, we want to keep connecting and bridging existing strategies, resources, and communities.
+
+Medium term, we want to keep expanding our reach, accounting for even more situational and cultural differences.
+
+Long term, there's opportunities to build tooling and infrastructure that would supercharge mentorship efforts.
+
+A wonderful, ongoing journey! 🎉
+
+## 👥 Sponsorship is important
+
+In addition to mentorship, sponsorship is important. There are plans to create a sponsorship guide as well. In the meantime, there is sponsorship related content in our [Resources](docs/resources-overview.md) section. Feel free to submit any helpful resources -- thank you! ✨
+
+## 💻 Local Development
+
+This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+yarn install
+```
+
+### Development
+
+```
+yarn start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+yarn build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Additional Resources
+
+Since this repo is built on Docusaurus, please reference the [Docusaurus 2 documentation](https://docusaurus.io/docs). Reach out if you have any questions. If this is also your first [contribution](docs/contributing.md) happy to help you get started!
---
+++ docusaurus.config.js
@@ -0,0 +1,136 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Index-Reup",
+  tagline: "No Anime No Life (Random skid by Necakco)",
+  url: "https://mentorship-guide.onrender.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "/favicon.ico",
+  organizationName: "mentorship-sponsorship",
+  projectName: "mentorship-guide-docs",
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+      },
+    ],
+  ],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/mentorship-sponsorship/mentorship-guide-docs/blob/main/",
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            "https://github.com/mentorship-sponsorship/mentorship-guide-docs/blob/main/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Mentorship Guide",
+        logo: {
+          alt: "Mentorship Guide Logo",
+          src: "img/mg-book.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "what-why-mentorship",
+            position: "left",
+            label: "Read",
+          },
+          { to: "/docs/contributing", label: "Contribute", position: "left" },
+          {
+            href: "https://github.com/mentorship-sponsorship/mentorship-guide-docs",
+            position: "right",
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Read",
+            items: [
+              {
+                label: "Why mentorship?",
+                to: "/docs/what-why-mentorship",
+              },
+              {
+                label: "For Mentors",
+                to: "/docs/mentors-overview",
+              },
+              {
+                label: "For Mentees",
+                to: "/docs/mentees-overview",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "About",
+                to: "/docs/about",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/mentorship-sponsorship/mentorship-guide-docs",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/holaguedis",
+              },
+            ],
+          },
+          {
+            title: "Contribute",
+            items: [
+              {
+                label: "What about sponsorship?",
+                to: "/docs/about#sponsorship-is-important",
+              },
+              {
+                label: "Share what you can, when you can, how you can",
+                to: "/docs/contributing",
+              },
+            ],
+          },
+        ],
+        copyright: `Mentorship Guide - Built with Docusaurus`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        disableSwitch: false,
+      },
+    }),
+};
+
+module.exports = config;
